21b8e38886be6840c480c848f0eae09a0c207ef8
Sunny <sunnylee945@gmail.com>
2017-03-29 22:58:58 +0800

    constants.cs

---
+++ share/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace share
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://sharecarenjoylife.azurewebsites.net";
+	}
+}
+
+
